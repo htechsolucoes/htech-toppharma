2aaef309c30e56ecc9baab7efc07f293047619ed
htechsolucoes <davi.calebe@htech-solucoes.com>
2026-08-10 17:13:00 -0300

    a

--- api/[...path].js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    message: "Function funcionando",
-    method: req.method,
-    query: req.query,
-    url: req.url
-  });
-}
